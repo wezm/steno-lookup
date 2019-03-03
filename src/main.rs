71ea3103b2dc88b5a10880b949d659138d4beae9
Wesley Moore <wes@wezm.net>
2019-03-03 20:18:02 +1100

    Add output format option and load each of the dictionaries

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use std::path::{Path, PathBuf};
 use directories::{ProjectDirs, UserDirs};
 use structopt::StructOpt;
 
-use steno_lookup::Error;
+use steno_lookup::{Dictionary, Error};
+
+#[derive(Debug)]
+enum OutputFormat {
+    Text,
+    Json,
+    Alfred,
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "steno-lookup", about = "Look up strokes for word.")]
@@ -31,13 +38,18 @@ struct Opt {
     #[structopt(short = "d", long = "dict", parse(from_os_str), number_of_values = 1)]
     dictionaries: Vec<PathBuf>,
 
+    /// The format in which results are printed
+    ///
+    /// `text` is the default.
+    #[structopt(long, short, raw(possible_values = "&[\"text\", \"json\", \"alfred\"]"))]
+    format: Option<String>,
+
     /// Word to look up
     search_term: String,
 }
 
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
 
     match run(&opt) {
         Ok(()) => (),
@@ -46,7 +58,7 @@ fn main() {
         Err(Error::Ini(err)) => eprintln!("INI error {}", err),
         Err(Error::FileNotFound(path)) => eprintln!("File not found: {}", path.to_string_lossy()),
         Err(Error::SectionMissing) => {
-            eprintln!("section '{}' not found in plove config", opt.section)
+            eprintln!("section '{}' not found in plover config", opt.section)
         }
         Err(Error::HomeNotFound) => eprintln!("Unable to dermine home directory"),
     }
@@ -55,6 +67,14 @@ fn main() {
 fn run(opt: &Opt) -> Result<(), Error> {
     // Build the list of dictionaries to load
     let dictionary_paths = dictionary_list(&opt)?;
+
+    // Need to load each of the dicts
+    let dictionaries = dictionary_paths
+        .iter()
+        .map(|path| Dictionary::load(path))
+        .collect::<Result<Vec<_>, Error>>();
+
+    dbg!(&dictionaries);
 
     Ok(())
 }
@@ -84,6 +104,19 @@ fn dictionary_list(opt: &Opt) -> Result<Vec<PathBuf>, Error> {
     dictionaries.append(&mut opt.dictionaries.clone());
 
     Ok(dictionaries)
+}
+
+impl From<&str> for OutputFormat {
+    fn from(s: &str) -> Self {
+        match s {
+            "text" => OutputFormat::Text,
+            "json" => OutputFormat::Json,
+            "alfred" => OutputFormat::Alfred,
+            // It is expected that StructOpt will have already sanitised the option so this should
+            // not happen unless there is a bug in the code.
+            _ => panic!("unexpected output format {}", s),
+        }
+    }
 }
 
 impl Opt {
